72d00474474029b577b4b7aacc7c0d97a5b8fb2e
Maksim Kanev <maks_kv@yahoo.com>
2020-08-06 16:50:42 +0300

    Added validation check for the datepicker

--- src/components/Dates.jsx
+++ src/components/Dates.jsx
@@ -8,32 +8,56 @@ class Dates extends Component {
         this.setStartDate = this.setStartDate.bind(this);
         this.setEndDate = this.setEndDate.bind(this);
         this.handleDateSubmit = this.handleDateSubmit.bind(this);
+        this.validateDate = this.validateDate.bind(this);
     };
 
     state = {
         startDate: new Date(),
         endDate: new Date(),
+
+        startDateValid: true,
+        endDateValid: true,
     };
 
     setStartDate(date) {
+        let check = this.validateDate(date);
+
         this.setState({
             startDate: date,
+            startDateValid: check,
         });
     };
 
     setEndDate(date) {
+        let check = this.validateDate(date);
+
         this.setState({
             endDate: date,
+            endDateValid: check,
         });
     };
 
     handleDateSubmit(event) {
         event.preventDefault();
 
-        if(this.state.startDate instanceof Date && this.state.endDate instanceof Date)
+        if(this.state.startDate instanceof Date && this.state.endDate instanceof Date
+            && this.state.startDateValid && this.state.endDateValid)
         {
             this.props.updateDates(this.state.startDate, this.state.endDate);
         }
+    };
+
+    validateDate(date) {     
+        let now = new Date();
+        now.setHours(0,0,0,0);
+
+        let check = false;
+
+        if (date > now) {
+            check = true;
+        } 
+
+        return check;
     };
 
     render() {
@@ -48,6 +72,9 @@ class Dates extends Component {
                     placeholderText="I have been cleared!"
                     />
                 </div>
+                <h4 className={this.state.startDateValid ? "validationPass" : "validationError"}>Start date is invalid!</h4>
+
+
                 <h3 style={{marginTop: "20px"}}>Pick an end date for your vacation</h3>
                 <DatePicker
                     selected={this.state.endDate}
@@ -55,6 +82,7 @@ class Dates extends Component {
                     isClearable
                     placeholderText="I have been cleared!"
                     />
+                <h4 className={this.state.endDateValid ? "validationPass" : "validationError"}>End date is invalid!</h4>
                 
                 <form onSubmit={this.handleDateSubmit} className="date-form">
                     <input type="submit" value="Save"></input>
